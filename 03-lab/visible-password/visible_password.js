c16b52d3d7037c6cbefa9ce6bd46e3dab57129ff
András Balázs Surek <37701012+absurek@users.noreply.github.com>
2023-03-29 14:23:06 +0000

    Lab 03 Added.

---
+++ 03-lab/visible-password/visible_password.js
@@ -0,0 +1,27 @@
+class VisiblePassword extends HTMLElement {
+  constructor() {
+    super();
+    const template = document.querySelector('template');
+    const content = template.content.cloneNode(true);
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    shadowRoot.appendChild(content);
+  }
+
+  connectedCallback() {
+    this.input = this.querySelector('input');
+    this.button = this.shadowRoot.querySelector('button');
+
+    this.button.addEventListener('click', (e) => this.toggleInputVisibility(e));
+  }
+
+  toggleInputVisibility(e) {
+    if (this.input.type === 'password') {
+      this.input.type = 'text';
+    } else {
+      this.input.type = 'password';
+    }
+    e.preventDefault();
+  }
+}
+
+window.customElements.define('visible-password', VisiblePassword);
